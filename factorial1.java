1764f065665e197c9b80e1bca31112f61f2e0cb3
Rmirza97 <137315222+Rmirza97@users.noreply.github.com>
2023-09-09 13:26:19 +0530

    Add files via upload

---
+++ factorial1.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class factorial1
+{
+public static void main(String args[])throws IOException;
+{
+DataInputStream br=new DataInputStream(system.in);
+system.out.println("Enter the number:");
+int No=Interger.parseInt(br.readLine());
+int Fact=1,i=1;
+for(i=1;i<=NO;i++)
+{
+fact=fact*i;
+}
+system.out.println("The factorial is");
+}
+}
